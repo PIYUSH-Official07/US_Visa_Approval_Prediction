db7eb5da93084a405d8ccc3de84491652316d418
Piyush S <singh.piyush575@gmail.com>
2024-05-08 15:14:52 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Piyush Singh",
+    author_email="singh.piyush575@gmail.com",
+    description="US Visa Approval Prediction Project",
+    packages=find_packages()
+)
